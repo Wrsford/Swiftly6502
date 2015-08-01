8858e91938b76134377a33519eb187d161501b64
Wrsford <willstafford@me.com>
2015-07-31 23:35:01 -0400

    Initial Commit

---
+++ Swiftly6502/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swiftly6502
+//
+//  Created by Will Stafford on 7/31/15.
+//  Copyright © 2015 Wrsford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
